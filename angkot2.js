0dc305c64fffb8e25f36d1d876eb1a19c93f3c99
isaljob <isaljob@gmail.com>
2019-10-24 08:07:44 +0700

    sebelum pull dari laptop dell

--- angkot2.js
+++ angkot2.js
@@ -4,9 +4,9 @@ var angkotBeroperasi = 6;
 
 while (noAngkot <= angkotBeroperasi) {
     console.log("Angkot No." + noAngkot + " beroperasi dengan baik.");
-noAngkot++;
+    noAngkot++;
 }
 
-for (noAngkot = angkotBeroperasi+1; noAngkot <= jmlAngkot; noAngkot++) {
+for (noAngkot = angkotBeroperasi + 1; noAngkot <= jmlAngkot; noAngkot++) {
     console.log("Angkot No." + noAngkot + " sedang tidak beroperasi.");
 }
--- angkot3.js
+++ angkot3.js
@@ -2,7 +2,7 @@ var noAngkot = 1;
 var jmlAngkot = 10;
 var angkotBeroperasi = 3;
 
-for (noAngkot ; noAngkot <= jmlAngkot; noAngkot++) {
+for (noAngkot; noAngkot <= jmlAngkot; noAngkot++) {
     if (noAngkot <= angkotBeroperasi) {
         console.log("Angkot No." + noAngkot + " beroperasi dengan baik.");
     } else {
